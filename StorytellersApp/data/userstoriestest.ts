f14ac3f9c79f2b3c2636bed777f5b2724228db71
AidanBrasseur <aidanbrasseur@gmail.com>
2020-11-12 18:30:11 -0500

    Modifying home screen UI with mocked audio player and mock data

---
+++ StorytellersApp/data/userstoriestest.ts
@@ -0,0 +1,57 @@
+const userstories = [
+    {
+        id: 1,
+        user: {
+            id: 1,
+            username: 'aidan',
+        },
+        creationTime: '2020-08-27T12:00:00.000Z',
+        title: 'test title',
+        description: 'Ut sed pretium orci. Nullam eget pharetra lacus. Nullam accumsan porttitor elit vitae commodo. Duis a felis semper, ultricies ante et, rhoncus urna. In sodales mi lacus, ut tincidunt quam maximus non. Ut placerat, metus at aliquam pharetra, arcu nulla ullamcorper velit, eget suscipit quam nulla ullamcorper sapien. Morbi fermentum risus sit amet eros ullamcorper porttitor. Etiam ac odio nibh. Integer non volutpat turpis. Pellentesque.',
+        recording: '',
+        numberOfReplies: 0,
+        numberOfLikes: 2,
+    },
+    {
+        id: 2,
+        user: {
+            id: 1,
+            username: 'aidan',
+        },
+        creationTime: '2020-08-27T12:00:00.000Z',
+        title: 'A Strange Night in New Brunswick',
+        description: 'Ut sed pretium orci. Nullam eget pharetra lacus. Nullam accumsan porttitor elit vitae commodo. Duis a felis semper, ultricies ante et, rhoncus urna. In sodales mi lacus, ut tincidunt quam maximus non. Ut placerat, metus at aliquam pharetra, arcu nulla ullamcorper velit, eget suscipit quam nulla ullamcorper sapien. Morbi fermentum risus sit amet eros ullamcorper porttitor. Etiam ac odio nibh. Integer non volutpat turpis. Pellentesque.',
+        recording: '',
+        numberOfReplies: 0,
+        numberOfLikes: 2,
+    },
+    {
+        id: 3,
+        user: {
+            id: 1,
+            username: 'aidan',
+        },
+        creationTime: '2020-08-27T12:00:00.000Z',
+        title: 'test title',
+        description: 'Ut sed pretium orci. Nullam eget pharetra lacus. Nullam accumsan porttitor elit vitae commodo. Duis a felis semper, ultricies ante et, rhoncus urna. In sodales mi lacus, ut tincidunt quam maximus non. Ut placerat, metus at aliquam pharetra, arcu nulla ullamcorper velit, eget suscipit quam nulla ullamcorper sapien. Morbi fermentum risus sit amet eros ullamcorper porttitor. Etiam ac odio nibh. Integer non volutpat turpis. Pellentesque.',
+        recording: '',
+        numberOfReplies: 0,
+        numberOfLikes: 2,
+    },
+    {
+        id: 4,
+        user: {
+            id: 1,
+            username: 'aidan',
+        },
+        creationTime: '2020-08-27T12:00:00.000Z',
+        title: 'test title',
+        description: 'Ut sed pretium orci. Nullam eget pharetra lacus. Nullam accumsan porttitor elit vitae commodo. Duis a felis semper, ultricies ante et, rhoncus urna. In sodales mi lacus, ut tincidunt quam maximus non. Ut placerat, metus at aliquam pharetra, arcu nulla ullamcorper velit, eget suscipit quam nulla ullamcorper sapien. Morbi fermentum risus sit amet eros ullamcorper porttitor. Etiam ac odio nibh. Integer non volutpat turpis. Pellentesque.',
+        recording: '',
+        numberOfReplies: 0,
+        numberOfLikes: 2,
+    },
+    
+];
+
+export default userstories;
