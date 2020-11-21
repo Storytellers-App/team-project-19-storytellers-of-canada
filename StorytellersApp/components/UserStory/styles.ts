dd2ea5fa2e6fa6158c57b181ffa8c3e37d3294fe
AidanBrasseur <aidanbrasseur@gmail.com>
2020-11-21 15:41:54 -0500

    Aligning name, username, date in card view

--- StorytellersApp/components/UserStory/styles.ts
+++ StorytellersApp/components/UserStory/styles.ts
@@ -21,17 +21,12 @@ const styles = StyleSheet.create({
     attribution: {
         margin: 12,
     },
-    author: {
-        marginHorizontal: 15,
-    },
     titleStyle: {
         marginLeft: 15,
         fontWeight: 'bold',
         fontSize: 20,
     },
-    dateStyle: {
-        alignItems: 'flex-end'
-    },
+
     row: {
         flex: 1,
         flexDirection: 'row',
@@ -48,10 +43,22 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
     },
     userRow: {
-        flex: 1,
         flexDirection: 'row',
-        alignItems: 'flex-start',
-    }
+    },
+    name: {
+        marginLeft: 15,
+        marginRight: 5,
+        color: 'grey',
+    },
+    username: {
+        marginRight: 5,
+        color: 'grey',
+    },
+    createdAt: {
+        marginLeft: 5,
+        marginRight: 5,
+        color: 'grey',
+    },
 });
 
 export default styles;
