7ac13da087e7b3f232ac813c746e664779ecc222
AidanBrasseur <aidanbrasseur@gmail.com>
2020-11-21 17:11:23 -0500

    Adding support for tags on stories

--- StorytellersApp/components/UserStory/index.tsx
+++ StorytellersApp/components/UserStory/index.tsx
@@ -29,6 +29,7 @@ import { UserStoryType, RootStackParamList } from '../../types';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { memo } from 'react';
+import Tags from '../Tags';
 export type UserStoryProps = {
     story: UserStoryType,
 }
@@ -37,36 +38,43 @@ import Footer from '../CardFooter';
 function UserStory(props: UserStoryProps) {
     const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
     const storyDetails = (story: UserStoryType) => {
-        navigation.push("StoryResponse", {'story': story});
+        navigation.push("StoryResponse", { 'story': story });
     }
     const colorScheme = useColorScheme();
     return (
-        <TouchableWithoutFeedback onPress={() => { storyDetails(props.story) }}>
-            <Card style={styles.card}>
-                <View style={[styles.row, styles.attribution,]}>
-                    <ProfilePicture size={42} />
-                    <View>
-                        <Text style={styles.titleStyle}
-                        >{props.story.title} </Text>
-                        <View style={styles.userRow}>
-                            <Text style={styles.name}>{props.story.user.name}</Text>
-                            <Text style={styles.username}>{props.story.user.username}</Text>
-                            <Text style={styles.createdAt} >{moment(props.story.creationTime).fromNow()}</Text>
 
+        <Card style={styles.card}>
+            <TouchableWithoutFeedback onPress={() => { storyDetails(props.story) }}>
+                <View>
+                    <View style={[styles.row, styles.attribution,]}>
+                        <ProfilePicture size={42} />
+                        <View>
+                            <Text style={styles.titleStyle}
+                            >{props.story.title} </Text>
+                            <View style={styles.userRow}>
+                                <Text style={styles.name}>{props.story.user.name}</Text>
+                                <Text style={styles.username}>{props.story.user.username}</Text>
+                                <Text style={styles.createdAt} >{moment(props.story.creationTime).fromNow()}</Text>
+
+                            </View>
                         </View>
-                    </View>
 
+                    </View>
+                    <Card.Content style={styles.content}>
+                        <Text>
+                            {props.story.description}
+                        </Text>
+                        <StoryPlayer />
+
+                    </Card.Content>
                 </View>
-                <Card.Content style={styles.content}>
-                    <Text>
-                        {props.story.description}
-                    </Text>
-                    <StoryPlayer />
-                </Card.Content>
-                <Divider />
-                <Footer story={props.story} />
-            </Card>
-        </TouchableWithoutFeedback>
+            </TouchableWithoutFeedback>
+            <Tags tags={props.story.tags}></Tags>
+            <Divider />
+
+            <Footer story={props.story} />
+        </Card>
+
     );
 }
 export default memo(UserStory);
