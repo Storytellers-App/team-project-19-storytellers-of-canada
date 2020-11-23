70511fdabe72e6af8efd52617db44bb4351675c1
Priyanshu Arora <priyarora4@gmail.com>
2020-11-22 22:14:01 -0500

    quick import fix new recording

--- StorytellersApp/components/NewRecordingButton/index.tsx
+++ StorytellersApp/components/NewRecordingButton/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import {Text, View} from '../components/Themed';
 
 import styles from './styles';
 import { useNavigation } from '@react-navigation/native';
