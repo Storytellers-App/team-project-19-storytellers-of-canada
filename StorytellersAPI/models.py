572e3e74b0a0823ddcf3d43c4e3b94a766696ac9
AidanBrasseur <aidanbrasseur@gmail.com>
2020-11-23 00:04:31 -0500

    Fixes to model

--- StorytellersAPI/models.py
+++ StorytellersAPI/models.py
@@ -28,12 +28,9 @@ class Story(db.Model):
     image = db.Column(db.String(500))
     type = db.Column(db.String(455), nullable=False,
                      default=StoryType.USER.value)
-    # this will be a temporary solution to create likes
-    # will need to add separate table later to store which user liked certain
-    # posts as well
     numLikes = db.Column(db.Integer)
     numReplies = db.Column(db.Integer)
-    approvedTime = db.Column(db.DateTime, nullable=False)
+    approvedTime = db.Column(db.DateTime)
 
 
 class Tag(db.Model):
@@ -53,17 +50,14 @@ class Comment(db.Model):
     approved = db.Column(db.Boolean, nullable=False, default=False)
     type = db.Column(db.String(455), nullable=False,
                      default='comment')
-    # this will be a temporary solution to create likes
-    # will need to add separate table later to store which user liked certain
-    # posts as well
     numLikes = db.Column(db.Integer)
     numReplies = db.Column(db.Integer)
-    approvedTime = db.Column(db.DateTime, nullable=False)
+    approvedTime = db.Column(db.DateTime)
 
 
 class Like(db.Model):
     __tablename__ = 'Likes'
-    postId = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(255))
-    postType = db.Column(db.String(455), nullable=False,
+    postId = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), primary_key=True)
+    postType = db.Column(db.String(455), primary_key=True,
                          default=StoryType.USER.value)
