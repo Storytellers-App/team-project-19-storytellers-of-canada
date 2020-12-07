daa84ee8ae1a8b910586dd83cdd2ae615f0b6784
James Currier <jcurrier@live.ca>
2020-12-06 23:44:28 -0500

    Add functional HELP button to Story Save Screen

--- StorytellersApp/screens/StorySaveScreen.tsx
+++ StorytellersApp/screens/StorySaveScreen.tsx
@@ -1,9 +1,11 @@
 import React, { Component, useEffect, useState } from 'react'
-import { SafeAreaView, View, StyleSheet, ScrollView, StatusBar, Image, Text, TextInput } from 'react-native';
+import { SafeAreaView, View, StyleSheet, ScrollView, StatusBar, Image, Text, TextInput, TouchableOpacity } from 'react-native';
 import StorySave from '../components/StorySave';
 import {
   Searchbar,
   Appbar,
+  Button,
+  Portal,
 } from 'react-native-paper';
 
 export default function StorySaveScreen() {
@@ -11,6 +13,7 @@ export default function StorySaveScreen() {
   const [searchText, setSearchText] = React.useState('');
   const [searchQuery, setSearchQuery] = React.useState('');
   const [focus, setFocus] = React.useState(false);
+  const [helpOpen, setHelpOpen] = React.useState(false);
 
   const onChangeSearch = (query: string) => {
     setSearchText(query);
@@ -42,9 +45,44 @@ export default function StorySaveScreen() {
             onEndEditing={() => setFocus(false)}
           />
         </View>
+        <Button
+          onPress={() => {
+            setHelpOpen(!helpOpen);
+          }}
+        >
+          HELP
+        </Button>
+        {helpOpen && (
+          <Portal>
+            <View style={styles.faded}>
+              <Text style={styles.message}>This is the Saved Story Collection</Text>
+            </View>
+            <TouchableOpacity
+              style={{ height: "100%" }}
+              onPress={() => setHelpOpen(false)}
+            />
+          </Portal>
+        )}
         {/* <Appbar.Action icon="dots-vertical"  />  */}
       </Appbar.Header>
       <StorySave key={searchQuery} search={searchQuery}></StorySave>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  faded: {
+    backgroundColor: '#00000099',
+    position: 'absolute',
+    zIndex: 0,
+    height: '100%',
+    width: '100%',
+},
+message: {
+    top: '15%',
+    margin: '6%',
+    padding: '2%',
+    backgroundColor: 'white',
+    textAlign: "center"
+},
+});
