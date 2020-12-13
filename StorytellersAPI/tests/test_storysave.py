1983f2d6575e80b0a36382cad87d296025552151
Tony (Ze) Yi <24402057+tony-ze-yi@users.noreply.github.com>
2020-12-13 18:06:20 -0500

    created storysave test

---
+++ StorytellersAPI/tests/test_storysave.py
@@ -0,0 +1,23 @@
+from app import app
+import unittest
+import json
+
+
+class TestHelloWorld(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_db(self):
+        response = self.app.get('/testdb')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_story(self):
+        response = self.app.get('/story_fetch?key=How Heart Came Into The World - Dan Yashinsky.mp3&bucket=sccanada')
+        response_json = json.loads(response.data)
+        self.assertEqual(len(response_json['url']) > 0, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
